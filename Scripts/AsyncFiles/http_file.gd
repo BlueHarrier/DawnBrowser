0692f26b13fd1375f91f891548f35d38dcf2a263
BlueHarrier <danipiriz9@gmail.com>
2024-05-19 14:13:01 +0200

    Fix: File risking being dereferenced, it's now kept alive until the request is complete

--- Scripts/AsyncFiles/http_file.gd
+++ Scripts/AsyncFiles/http_file.gd
@@ -10,13 +10,17 @@ func _load() -> void:
         _request.cancel_request()
         _request.queue_free()
     _request = SystemManager.create_http_request()
-    _request.request_completed.connect(__on_request_completed)
-    _request.request(uri)
-
-func __on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-    _request.queue_free()
-    if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
+    _request.request_completed.connect(_on_request_completed)
+    if _request.request(uri) != OK:
         file_error.emit(ERR_CANT_ACQUIRE_RESOURCE)
+        return
+    reference() # Recursive reference to prevent deletion
+
+func _on_request_completed(result: int, _response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+    _request.queue_free()
+    if result != HTTPRequest.RESULT_SUCCESS:
+        file_error.emit(ERR_CANT_ACQUIRE_RESOURCE)
+        unreference() # Unreference to allow deletion
         return
     for header in headers:
         var header_split: PackedStringArray = header.split(":")
@@ -27,11 +31,17 @@ func __on_request_completed(result: int, response_code: int, headers: PackedStri
                 var mime_type: PackedStringArray = header_split[1].split("/")
                 type = mime_type[1]
                 name = uri.get_file() + "." + type
+    var dir: DirAccess = DirAccess.open("user://")
+    if !dir.dir_exists("temp"):
+        dir.make_dir("temp")
     var unique_name: String = "user://temp/" + __generate_uuid() + "." + type
+    while FileAccess.file_exists(unique_name):
+        unique_name = "user://temp/" + __generate_uuid() + "." + type
     var file_access: FileAccess = FileAccess.open(unique_name, FileAccess.WRITE)
     file_access.store_buffer(body)
     file_access.close()
     file_loaded.emit(unique_name)
+    unreference() # Unreference to allow deletion
 
 func __generate_uuid() -> String:
     var uuid: String = ""
