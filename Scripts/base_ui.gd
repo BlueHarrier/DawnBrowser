8cb6efde41de6d60c908e69d9b60ed3e175f30ce
BlueHarrier <danipiriz9@gmail.com>
2024-05-19 12:46:44 +0200

    Add: LocalFile load

--- Scripts/base_ui.gd
+++ Scripts/base_ui.gd
@@ -13,29 +13,25 @@ func on_load_button_pressed() -> void:
     var path: String = path_bar.text
     if path == "":
         return
-    var path_split: PackedStringArray = path.split("#")
-    var scene: Variant = null
-    if path_split.size() >= 2:
-        if path_split[1].is_valid_int():
-            scene = path_split[1].to_int()
-        else:
-            scene = path_split[1]
-    path = path_split[0]
+    load_button.disabled = true
+    var file: AsyncFile = LocalFile.new(path)
+    file.bind_signals(load_gltf, load_error)
+    file.load()
+
+func load_gltf(path: String) -> void:
     var itf: ITFDocument = ITFDocument.new()
     var error: Error = itf.open(path)
     if error != OK:
         printerr("Error loading ITF file: " + str(error))
         return
-    var world_scene: PackedScene = null
-    if scene:
-        if scene is int:
-            itf.get_world(scene)
-        else:
-            itf.get_world_by_name(scene)
-    else:
-        world_scene = itf.get_default_world()
+    var world_scene: PackedScene = itf.get_default_world()
     if world_scene == null:
         printerr("No world found in ITF file")
         return
     var system_manager: _SystemManager = SystemManager
     system_manager.load_world(world_scene)
+    load_button.disabled = false
+
+func load_error(error: Error) -> void:
+    printerr("Error loading file: " + str(error))
+    load_button.disabled = false
